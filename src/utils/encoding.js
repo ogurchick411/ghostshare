a680554647d7f057d9846bbe6203129232557ea5
Ogurchick <148265956+ogurchick411@users.noreply.github.com>
2026-07-27 18:34:53 +0200

    implement LSB steganography encoder and decoder

---
+++ src/utils/encoding.js
@@ -0,0 +1,17 @@
+export function bufferToBase64(buffer) {
+  const bytes = new Uint8Array(buffer);
+  let binary = "";
+  for (let i = 0; i < bytes.byteLength; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary);
+}
+
+export function base64ToBuffer(base64) {
+  const binary = atob(base64);
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < binary.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
